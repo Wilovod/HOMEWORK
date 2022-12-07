f7c04e6c88ed7fb3979b5ac2bd9bc0c4e37a59c7
Wilovod <wilovod@gmail.com>
2022-12-07 22:14:30 +0700

    add Work12

---
+++ Work12/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+SumNum(num);
+
+Console.WriteLine($"Сумма: {sum} ");
+
+int SumNum(int a)
+{
+    while (a > 0)
+{
+    sum = sum + a % 10;
+    a = a / 10;
+}
+    return sum;
+}
